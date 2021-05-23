117af9d53d5c0a3ff2476f0ee2a08910530f585e
sotiriosmos <sotiriosmos@gmail.com>
2021-05-23 15:53:49 +0300

    Question 2 added

--- HW.4/Ex4.m
+++ HW.4/Ex4.m
@@ -22,61 +22,110 @@ y5f=audioread('U-Female.m4a');
 
 % Plots of time domain signals
 
-t=(0:length(y1m)-1)/Fs;
-plot(t,y1m)
+t1=(0:length(y1m)-1)/Fs;
+plot(t1,y1m(:,2))
 title('Time domain signal of "A" Male')
 xlabel('Time(s)')
 
 figure;
-t=(0:length(y2m)-1)/Fs;
-plot(t,y2m)
+t2=(0:length(y2m)-1)/Fs;
+plot(t2,y2m(:,2))
 title('Time domain signal of "E" Male')
 xlabel('Time(s)')
 
 figure;
-t=(0:length(y3m)-1)/Fs;
-plot(t,y3m)
+t3=(0:length(y3m)-1)/Fs;
+plot(t3,y3m(:,2))
 title('Time domain signal of "I" Male')
 xlabel('Time(s)')
 
 figure;
-t=(0:length(y4m)-1)/Fs;
-plot(t,y4m)
+t4=(0:length(y4m)-1)/Fs;
+plot(t4,y4m(:,2))
 title('Time domain signal of "O" Male')
 xlabel('Time(s)')
 
 figure;
-t=(0:length(y5m)-1)/Fs;
-plot(t,y5m)
+t5=(0:length(y5m)-1)/Fs;
+plot(t5,y5m(:,2))
 title('Time domain signal of "U" Male')
 xlabel('Time(s)')
 
 figure;
-t=(0:length(y1f)-1)/Fs;
-plot(t,y1f)
+t6=(0:length(y1f)-1)/Fs;
+plot(t6,y1f(:,2))
 title('Time domain signal of "A" Female')
 xlabel('Time(s)')
 
 figure;
-t=(0:length(y2f)-1)/Fs;
-plot(t,y2f)
+t7=(0:length(y2f)-1)/Fs;
+plot(t7,y2f(:,2))
 title('Time domain signal of "E" Female')
 xlabel('Time(s)')
 
 figure;
-t=(0:length(y3f)-1)/Fs;
-plot(t,y3f)
+t8=(0:length(y3f)-1)/Fs;
+plot(t8,y3f(:,2))
 title('Time domain signal of "I" Female')
 xlabel('Time(s)')
 
 figure;
-t=(0:length(y4f)-1)/Fs;
-plot(t,y4f)
+t9=(0:length(y4f)-1)/Fs;
+plot(t9,y4f(:,2))
 title('Time domain signal of "O" Female')
 xlabel('Time(s)')
 
 figure;
-t=(0:length(y5f)-1)/Fs;
-plot(t,y5f)
+t10=(0:length(y5f)-1)/Fs;
+plot(t10,y5f(:,2))
 title('Time domain signal of "U" Female')
 xlabel('Time(s)')
+
+% Real Cepstrum of each voice signal
+c1m=rceps(y1m(:,2));
+c1f=rceps(y1f(:,2));
+figure;
+plot(c1m,'blue');
+hold on
+plot(c1f,'red');
+legend('"A"-Male','"A"-Female')
+
+c2m=rceps(y2m(:,2));
+c2f=rceps(y2f(:,2));
+figure;
+plot(c2m,'blue');
+hold on
+plot(c2f,'red');
+legend('"E"-Male','"E"-Female')
+
+c2m=rceps(y2m(:,2));
+c2f=rceps(y2f(:,2));
+figure;
+plot(c2m,'blue');
+hold on
+plot(c2f,'red');
+legend('"E"-Male','"E"-Female')
+
+c3m=rceps(y3m(:,2));
+c3f=rceps(y3f(:,2));
+figure;
+plot(c3m,'blue');
+hold on
+plot(c3f,'red');
+legend('"I"-Male','"I"-Female')
+
+c4m=rceps(y4m(:,2));
+c4f=rceps(y4f(:,2));
+figure;
+plot(c4m,'blue');
+hold on
+plot(c4f,'red');
+legend('"O"-Male','"O"-Female')
+
+c5m=rceps(y5m(:,2));
+c5f=rceps(y5f(:,2));
+figure;
+plot(c5m,'blue');
+hold on
+plot(c5f,'red');
+legend('"U"-Male','"U"-Female')
